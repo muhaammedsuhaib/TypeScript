75bf83bf16017ae0e45fa33917abe94e4a29c80d
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-06-29 09:53:13 +0530

    first commit

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var heading = document.querySelector('h1');
+if (heading) {
+    heading.textContent = 'Hello, TypeScript and HTML!';
+}
+var descrption = document.querySelector('p');
+if (descrption) {
+    descrption.textContent = 'This is script language';
+}
+var a = 'suhaib';
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+const heading = document.querySelector('h1');
+if (heading) {
+  heading.textContent = 'Hello, TypeScript and HTML!';
+}
+const descrption = document.querySelector('p');
+if(descrption){
+  descrption.textContent = 'This is script language';
+}
+const a: string = 'suhaib';
